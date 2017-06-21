7874d8631b40d845ae329607011ea0c6b3f5089e
tiench189 <caotien189@gmail.com>
2017-06-21 08:42:51 +0700

    create respo

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tienc
+ * Date: 6/20/2017
+ * Time: 3:13 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    public function index(Request $request){
+        $user = DB::table('user')->get();
+        return response()->json($user);
+    }
+}
