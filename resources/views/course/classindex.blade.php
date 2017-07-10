0d6988ec2336962b6d1d4a572de237b84ce2a651
dtm273 <maimai273>
2017-07-10 09:34:17 +0700

    xoa lop hoc

--- resources/views/course/classindex.blade.php
+++ resources/views/course/classindex.blade.php
@@ -4,6 +4,24 @@
     Danh sách lớp học | {{ $course->fullname }}
 @stop
 @section('content')
+    {!! Form::open(array('route' => 'class-remove', 'class' => 'form', 'id' => 'frmClassDelete')) !!}
+    {{ Form::hidden('id', 0, array('id' => 'id')) }}
+    {{ Form::hidden('courseId', 0, array('id' => 'courseId')) }}
+    {!! Form::close() !!}
+
+    <script language="javascript">
+        function xoalophoc(id, courseId) {
+            if (confirm("Bạn có muốn xóa?")) {
+                document.getElementById("id").value = id;
+                document.getElementById("courseId").value = courseId;
+                frmClassDelete.submit();
+            }
+        }
+    </script>
+    @if (Session::has('message'))
+        <div class="alert alert-info">{!!  Session::get('message') !!}</div>
+    @endif
+
     <div class="page-title">Danh sách lớp <strong>{{ $course->fullname }}</strong></div>
     @if(\App\Roles::checkRole('class-edit'))
         <a class="btn btn-primary btn-add" href="{{route('class-edit', ['cid' => $course->id])}}" style="margin-bottom: 10px">Thêm
@@ -59,6 +77,13 @@
                            class="btn btn-xs btn-primary">
                             Cập nhật
                         </a>
+                    </td>
+                @endif
+                @if(\App\Roles::checkRole('class-remove'))
+                    <td>
+                        <a href="javascript:void(0)"
+                           onclick="xoalophoc({{$row->id}}, {{$row->course_id}})"
+                           class="btn btn-xs btn-primary">Xóa</a>
                     </td>
                 @endif
             </tr>
